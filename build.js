1aeeeb17fc9dcc39fa2e7d46e36040853e38c820
Flaki <git@flaki.hu>
2017-07-17 10:01:14 +0200

    Create separate compiler module

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ const translate = require('./modules/translate.js');
 const lookup = require('./modules/lookup.js');
 const getString = require('./modules/getString.js');
 
-const PixelData = require('../editor/lib/pif/lib.js')
+const PixelData = require('./node_modules/microcanvas-pixeldata/pixeldata.js');
 
 let srcFile = process.argv[2] || './game.js';
 let targetSystem = process.argv[3] || 'arduboy';
